923fc10436eaeca0f5fda35a56230c7a4db90d96
Hidetake Iwata <int128@gmail.com>
2024-11-01 19:12:45 +0900

    Refactor

--- src/run.ts
+++ src/run.ts
@@ -4,21 +4,20 @@ import * as github from './github.js'
 type Inputs = {
   sha: string
   event: string
-  owner: string
-  repo: string
   token: string
 }
 
-export const run = async (inputs: Inputs): Promise<void> => {
+export const run = async (inputs: Inputs, context: github.Context): Promise<void> => {
   const octokit = github.getOctokit(inputs.token)
 
-  core.info(`Fetching the failed workflow runs for ${inputs.owner}/${inputs.repo}/${inputs.event}@${inputs.sha}`)
+  core.info(`Fetching the failed workflow runs for ${context.owner}/${context.repo}@${inputs.sha}:${inputs.event}`)
   const workflowRuns = await octokit.paginate(octokit.rest.actions.listWorkflowRunsForRepo, {
-    owner: inputs.owner,
-    repo: inputs.repo,
+    owner: context.owner,
+    repo: context.repo,
     head_sha: inputs.sha,
     event: inputs.event,
     status: 'failure',
+    exclude_pull_requests: true,
     per_page: 100,
   })
   core.info(`Found ${workflowRuns.length} failed workflow runs`)
@@ -27,6 +26,7 @@ export const run = async (inputs: Inputs): Promise<void> => {
   core.summary.addTable([
     [
       { data: 'Workflow', header: true },
+      { data: 'Event', header: true },
       { data: 'Conclusion', header: true },
       { data: 'Status', header: true },
       { data: 'Attempt', header: true },
@@ -35,6 +35,7 @@ export const run = async (inputs: Inputs): Promise<void> => {
     ],
     ...workflowRuns.map((workflowRun) => [
       `<a href="${workflowRun.html_url}">${workflowRun.name ?? '-'}</a>`,
+      workflowRun.event,
       workflowRun.conclusion ?? '-',
       workflowRun.status ?? '-',
       workflowRun.run_attempt?.toString() ?? '-',
@@ -47,8 +48,8 @@ export const run = async (inputs: Inputs): Promise<void> => {
   for (const workflowRun of workflowRuns) {
     core.info(`Re-running failed workflow run: ${workflowRun.html_url}`)
     await octokit.rest.actions.reRunWorkflowFailedJobs({
-      owner: inputs.owner,
-      repo: inputs.repo,
+      owner: context.owner,
+      repo: context.repo,
       run_id: workflowRun.id,
     })
   }
