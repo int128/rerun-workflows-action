8d907808f8683fa76849f76aa433f5354b6093d2
Hidetake Iwata <int128@gmail.com>
2025-04-04 09:07:34 +0900

    Migrate to @octokit/action (#128)

--- src/github.ts
+++ src/github.ts
@@ -1,11 +1,29 @@
-import * as github from '@actions/github'
+import assert from 'assert'
+import { Octokit } from '@octokit/action'
 import { retry } from '@octokit/plugin-retry'
 
-export type Octokit = ReturnType<typeof github.getOctokit>
-
-export const getOctokit = (token: string): Octokit => github.getOctokit(token, {}, retry)
+export const getOctokit = () => new (Octokit.plugin(retry))()
 
 export type Context = {
-  owner: string
-  repo: string
+  repo: {
+    owner: string
+    repo: string
+  }
+}
+
+export const getContext = (): Context => {
+  // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+  return {
+    repo: getRepo(),
+  }
+}
+
+const getRepo = () => {
+  const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/')
+  return { owner, repo }
+}
+
+const getEnv = (name: string): string => {
+  assert(process.env[name], `${name} is required`)
+  return process.env[name]
 }
