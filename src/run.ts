78fe8d502f452db51abb07743700426daf812a68
Hidetake Iwata <int128@gmail.com>
2025-04-20 17:20:12 +0900

    Rerun all of failed workflow runs even if error (#152)
    * Rerun all workflow runs even if error
    
    * Refactor

--- src/run.ts
+++ src/run.ts
@@ -13,11 +13,11 @@ type Outputs = {
 
 export const run = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<Outputs> => {
   if (inputs.sha && inputs.event) {
-    return await rerunFailedWorkflows(inputs, octokit, context)
+    return await rerunFailedWorkflowRuns(inputs, octokit, context)
   }
 
   if ('pull_request' in context.payload) {
-    return await rerunFailedWorkflows(
+    return await rerunFailedWorkflowRuns(
       {
         event: 'pull_request',
         sha: context.payload.pull_request.head.sha,
@@ -34,7 +34,7 @@ export const run = async (inputs: Inputs, octokit: Octokit, context: Context): P
       repo: context.repo.repo,
       pull_number: context.payload.issue.number,
     })
-    return await rerunFailedWorkflows(
+    return await rerunFailedWorkflowRuns(
       {
         event: 'pull_request',
         sha: pull.head.sha,
@@ -48,22 +48,33 @@ export const run = async (inputs: Inputs, octokit: Octokit, context: Context): P
   return { workflowRunsCount: 0 }
 }
 
-const rerunFailedWorkflows = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<Outputs> => {
-  core.info(
-    `Finding the failed workflow runs for ${context.repo.owner}/${context.repo.repo}@${inputs.sha}:${inputs.event}`,
-  )
-  const workflowRuns = await octokit.paginate(octokit.rest.actions.listWorkflowRunsForRepo, {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    head_sha: inputs.sha,
-    event: inputs.event,
-    status: 'failure',
-    exclude_pull_requests: true,
-    per_page: 100,
-  })
-  core.info(`Found ${workflowRuns.length} failed workflow runs`)
+const rerunFailedWorkflowRuns = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<Outputs> => {
+  const workflowRuns = await findWorkflowRunsForRerun(inputs, octokit, context)
+  if (workflowRuns.length === 0) {
+    return { workflowRunsCount: 0 }
+  }
 
-  core.summary.addHeading('Workflow runs')
+  const rerunWorkflowRuns = []
+  for (const workflowRun of workflowRuns) {
+    core.info(`Rerunning: ${workflowRun.name}: ${workflowRun.html_url}`)
+    try {
+      await octokit.rest.actions.reRunWorkflowFailedJobs({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: workflowRun.id,
+      })
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        core.error(`Failed to rerun: ${error}`)
+        rerunWorkflowRuns.push({ workflowRun, error })
+        continue
+      }
+      throw error
+    }
+    rerunWorkflowRuns.push({ workflowRun })
+  }
+
+  core.summary.addHeading('rerun-workflow-runs-action summary', 2)
   core.summary.addTable([
     [
       { data: 'Workflow', header: true },
@@ -72,35 +83,46 @@ const rerunFailedWorkflows = async (inputs: Inputs, octokit: Octokit, context: C
       { data: 'Status', header: true },
       { data: 'Attempt', header: true },
       { data: 'Created at', header: true },
-      { data: 'Updated at', header: true },
+      { data: 'Rerun', header: true },
     ],
-    ...workflowRuns.map((workflowRun) => [
+    ...rerunWorkflowRuns.map(({ workflowRun, error }) => [
       `<a href="${workflowRun.html_url}">${workflowRun.name ?? '-'}</a>`,
       workflowRun.event,
       workflowRun.conclusion ?? '-',
       workflowRun.status ?? '-',
       workflowRun.run_attempt?.toString() ?? '-',
       new Date(workflowRun.created_at).toISOString(),
-      new Date(workflowRun.updated_at).toISOString(),
+      error ? `❌ ${error}` : `✅`,
     ]),
   ])
   await core.summary.write()
 
-  for (const workflowRun of workflowRuns) {
-    // Avoid re-running the current workflow to prevent an infinite loop.
-    if (workflowRun.name === context.workflow) {
-      continue
-    }
-
-    core.info(`Re-running failed workflow run: ${workflowRun.html_url}`)
-    await octokit.rest.actions.reRunWorkflowFailedJobs({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      run_id: workflowRun.id,
-    })
+  const errors = rerunWorkflowRuns.filter(({ error }) => error)
+  if (errors.length > 0) {
+    throw new Error(
+      `Failed to rerun some workflow runs. ` +
+        `See ${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId} for details.`,
+    )
   }
-
   return {
     workflowRunsCount: workflowRuns.length,
   }
+}
+
+const findWorkflowRunsForRerun = async (inputs: Inputs, octokit: Octokit, context: Context) => {
+  core.info(`Finding the failed workflow runs for commit ${inputs.sha} and event ${inputs.event}`)
+  const rawWorkflowRuns = await octokit.paginate(octokit.rest.actions.listWorkflowRunsForRepo, {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    head_sha: inputs.sha,
+    event: inputs.event,
+    status: 'failure',
+    exclude_pull_requests: true,
+    per_page: 100,
+  })
+
+  // Avoid re-running the current workflow to prevent an infinite loop.
+  const workflowRun = rawWorkflowRuns.filter((workflowRun) => workflowRun.name !== context.workflow)
+  core.info(`Found ${rawWorkflowRuns.length} failed workflow runs`)
+  return workflowRun
 }
